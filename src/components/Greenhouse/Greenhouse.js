5cab0b4a9c5bf8ab2383c19d4acc07c4142fa423
Naim Miah <mmiah@fordham.edu>
2023-09-05 13:20:10 -0400

    completed phasee one

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,19 +1,27 @@
-import dayImage from './images/greenhouse-day.jpg';
-import nightImage from './images/greenhouse-night.jpg';
-import './Greenhouse.css';
+import dayImage from "./images/greenhouse-day.jpg";
+import nightImage from "./images/greenhouse-night.jpg";
+import "./Greenhouse.css";
 
-import LightSwitch from './LightSwitch';
-import ClimateStats from './ClimateStats';
+import LightSwitch from "./LightSwitch";
+import ClimateStats from "./ClimateStats";
+import { useTheme } from "../../context/ThemeContext";
 
 function Greenhouse() {
-
+  const { themeName, setThemeName } = useTheme();
+  console.log("themeName", themeName);
+  const dayOrNight = themeName === "day" ? dayImage : nightImage;
   return (
     <section>
-      <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
+      <div>
+        {themeName === "day" ? (
+          <img className="greenhouse-img" src={dayImage} alt="greenhouse" />
+        ) : (
+          <img className="greenhouse-img" src={nightImage} alt="greenhouse" />
+        )}
+      </div>
+      <LightSwitch
+        onClick={() => setThemeName(themeName === "day" ? "night" : "day")}
       />
-      <LightSwitch />
       <ClimateStats />
     </section>
   );
